a6cf4f869f316ad9681d001c39998781bcb6e17c
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-09-12 21:30:29 +0100

    Update validate.js

--- learn-express/validate.js
+++ learn-express/validate.js
@@ -3,7 +3,7 @@ const Joi = require('joi')
 function ValidateUser(user) {
     const schema = Joi.object({
         name: Joi.string().min(3).required(),
-        age: Joi.number().min(18).required()
+        age: Joi.number().min(0).required()
     })
 
     return schema.validate(user)
